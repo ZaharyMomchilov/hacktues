72e64ccabc0d91a77d2d429f94b874e1e08663a4
ZaharyMomchilov <36733036+ZaharyMomchilov@users.noreply.github.com>
2021-02-05 11:44:34 +0200

    forgotten password

--- frontend/pages/change_password.js
+++ frontend/pages/change_password.js
@@ -22,63 +22,123 @@ export default function Login({logIn}) {
 	var router = useRouter()
 	const toast = useToast()
 
-	console.log(router.query);
-
-	return(
-		<Box marginLeft="15px" marginRight="15px">
-	  	<Box margin="auto" w={["100%","100%","25%","25%"]} minWidth={["none","none","35rem","35rem"]} backgroundColor="white" p="25px" mt="50px" rounded="lg">
-			<Formik initialValues={{ email: "" }} 
-	onSubmit={(values, actions) => {
-		setTimeout(() => {
-				console.log(values.email)
-				// var data = JSON.stringify(values, null, 1)
-				axios({
-					method: 'post',
-					url: `https://${process.env.hostname}/users/forgotten_password/`,
-					headers: 
-					{ "Content-type": "Application/json",
-					"Authorization": `Bearer ${cookies.get('auth')}`
-
-					},
-					data: {"email":values.email}  
-					  },)
-					.then(function (response) {
-						toast({ title: "Промяна на парола.", description: "Влезте в имейлът Ви, за да смените паролата.",status: "success", duration: 9000})
-						router.push('/')
-					})
-					.catch(function (error) {
-						if (error.response) {
-							toast({
-        						title: "Влизането не е успешно.",
-        						description: error.response.data.detail,
-        						status: "error",
-        						duration: 9000
-        					})
-						}})
-								console.log(JSON.stringify(values, null, 1))
-								actions.setSubmitting(false)
-							}, 1000);
-					}}>
-{props => (
-		<form onSubmit={props.handleSubmit}>
-		<Field name="email">
-			{({ field, form }) => (
-			  	<FormControl isRequired isInvalid={form.errors.email && form.touched.email}>
-					<FormLabel color="black" paddingBottom="5px" fontFamily="Rubik" fontSize="15px" htmlFor="text">
-							E-mail
-					</FormLabel>
-					<Input _focus={{outline:"none"}} outline="lightgrey" variant="outline" {...field} id="email1" />
-					<FormErrorMessage>{form.errors.email}</FormErrorMessage>
-			  	</FormControl>
+	if(router.query.token & router.query.token_id){
+		return(
+			<Box marginLeft="15px" marginRight="15px">
+			  <Box margin="auto" w={["100%","100%","25%","25%"]} minWidth={["none","none","35rem","35rem"]} backgroundColor="white" p="25px" mt="50px" rounded="lg">
+				<Formik initialValues={{ email: "" }} 
+		onSubmit={(values, actions) => {
+			setTimeout(() => {
+					console.log(values.email)
+					// var data = JSON.stringify(values, null, 1)
+					axios({
+						method: 'post',
+						url: `https://${process.env.hostname}/users/forgotten_password/`,
+						headers: 
+						{ "Content-type": "Application/json",
+						"Authorization": `Bearer ${cookies.get('auth')}`
+	
+						},
+						data: {"password":values.password,
+								"token_id":router.query.token_id,
+								"token":router.query.token
+						}  
+						  },)
+						.then(function (response) {
+							toast({ title: "Промяна на парола.", description: "Влезте в имейлът Ви, за да смените паролата.",status: "success", duration: 9000})
+							router.push('/')
+						})
+						.catch(function (error) {
+							if (error.response) {
+								toast({
+									title: "Влизането не е успешно.",
+									description: error.response.data.detail,
+									status: "error",
+									duration: 9000
+								})
+							}})
+									console.log(JSON.stringify(values, null, 1))
+									actions.setSubmitting(false)
+								}, 1000);
+						}}>
+	{props => (
+			<form onSubmit={props.handleSubmit}>
+			<Field name="password">
+				{({ field, form }) => (
+					  <FormControl isRequired isInvalid={form.errors.email && form.touched.email}>
+						<FormLabel color="black" paddingBottom="5px" fontFamily="Rubik" fontSize="15px" htmlFor="text">
+								Нова парола
+						</FormLabel>
+						<Input _focus={{outline:"none"}} outline="lightgrey" variant="outline" {...field} id="email1" />
+						<FormErrorMessage>{form.errors.email}</FormErrorMessage>
+					  </FormControl>
+				)}
+			</Field>
+			<Button mt={4} colorScheme="green" border="0" isLoading={props.isSubmitting} type="submit">
+				Смени паролата
+			</Button>
+			</form>
 			)}
-		</Field>
-		<Button mt={4} colorScheme="green" border="0" isLoading={props.isSubmitting} type="submit">
-			Смени паролата
-		</Button>
-		</form>
-		)}
-		</Formik>
-	</Box>
-	</Box>
-	)
+			</Formik>
+		</Box>
+		</Box>)
+	}
+	else{
+		return(
+			<Box marginLeft="15px" marginRight="15px">
+			  <Box margin="auto" w={["100%","100%","25%","25%"]} minWidth={["none","none","35rem","35rem"]} backgroundColor="white" p="25px" mt="50px" rounded="lg">
+				<Formik initialValues={{ email: "" }} 
+		onSubmit={(values, actions) => {
+			setTimeout(() => {
+					console.log(values.email)
+					// var data = JSON.stringify(values, null, 1)
+					axios({
+						method: 'post',
+						url: `https://${process.env.hostname}/users/forgotten_password/`,
+						headers: 
+						{ "Content-type": "Application/json",
+						"Authorization": `Bearer ${cookies.get('auth')}`
+	
+						},
+						data: {"email":values.email}  
+						  },)
+						.then(function (response) {
+							toast({ title: "Промяна на парола.", description: "Влезте в имейлът Ви, за да смените паролата.",status: "success", duration: 9000})
+							router.push('/')
+						})
+						.catch(function (error) {
+							if (error.response) {
+								toast({
+									title: "Влизането не е успешно.",
+									description: error.response.data.detail,
+									status: "error",
+									duration: 9000
+								})
+							}})
+									console.log(JSON.stringify(values, null, 1))
+									actions.setSubmitting(false)
+								}, 1000);
+						}}>
+	{props => (
+			<form onSubmit={props.handleSubmit}>
+			<Field name="email">
+				{({ field, form }) => (
+					  <FormControl isRequired isInvalid={form.errors.email && form.touched.email}>
+						<FormLabel color="black" paddingBottom="5px" fontFamily="Rubik" fontSize="15px" htmlFor="text">
+								E-mail
+						</FormLabel>
+						<Input _focus={{outline:"none"}} outline="lightgrey" variant="outline" {...field} id="email1" />
+						<FormErrorMessage>{form.errors.email}</FormErrorMessage>
+					  </FormControl>
+				)}
+			</Field>
+			<Button mt={4} colorScheme="green" border="0" isLoading={props.isSubmitting} type="submit">
+				Смени паролата
+			</Button>
+			</form>
+			)}
+			</Formik>
+		</Box>
+		</Box>)
+	}
 }
