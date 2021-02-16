f39b30aa31349e26800c41e15b84452e398af1ef
ZaharyMomchilov <36733036+ZaharyMomchilov@users.noreply.github.com>
2021-02-16 13:08:43 +0200

    fix

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -326,7 +326,7 @@ function MyApp({ Component, pageProps }) {
             content="initial-scale=1.0, width=device-width"
             
           />
-          <link rel="icon" type="image/x-icon" href="./favicon.ico" />
+          <link rel="icon" href="http://www.hacktues.com/favicon.ico?v=2" />
         </Head>
         <NextNprogress
           color="#009d60"
