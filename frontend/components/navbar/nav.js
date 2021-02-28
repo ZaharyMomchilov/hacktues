34ccb09384dc83e795886bdfeb192c2b7d58b0b5
ZaharyMomchilov <36733036+ZaharyMomchilov@users.noreply.github.com>
2021-02-28 12:16:13 +0200

    winners

--- frontend/components/navbar/nav.js
+++ frontend/components/navbar/nav.js
@@ -12,6 +12,7 @@ import { RiTeamLine } from "react-icons/ri";
 import { FiArchive } from "react-icons/fi";
 import { FaRegHandshake, FaListOl } from 'react-icons/fa'
 import {
+  AiOutlineTrophy,
   AiOutlineTeam,
   AiOutlineUserAdd,
   AiOutlineUsergroupAdd,
@@ -103,6 +104,7 @@ export default function Navigation(props) {
         {/* <MenuItem name="Теми" icon={PhoneIcon} link="/themes"/> */}
         <MenuItem name="Ментори" icon={FaRegHandshake} link="/mentors"/>
         <MenuItem name="Класации" icon={FaListOl } link="/placements"/>
+        <MenuItem name="Победители" icon={AiOutlineTrophy } link="/winners"/>
         <MenuItem name="Отбори" top="-1px" icon={RiTeamLine} link="/teams/"/>
         <MenuItem name="Архив" icon={FiArchive} link="/archive/" />
         <MenuItem name="Регламент" icon={GrDocumentText} link="/regulation/" />
